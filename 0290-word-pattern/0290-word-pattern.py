f5ce607d0d7ae5f6912fc4d97e3f6f7038f59bc6
AkiMathur <90270340+AkiMathur@users.noreply.github.com>
2025-11-01 13:51:54 +0530

    Time: 0 ms (100%), Space: 18 MB (15.64%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+        hash_p = dict()
+        len_p = len(pattern)
+        str_p = []
+
+        if len_p != len(s):
+            return False
+            
+        for i in range(len_p):
+            if pattern[i] not in hash_p.keys():
+                if s[i] not in hash_p.values():
+                    hash_p[pattern[i]] = s[i]
+                else:
+                    return False 
+
+        for i in pattern:
+            str_p.append(hash_p[i])
+        
+
+        if str_p == s:
+            return True
+        else:
+            return False
